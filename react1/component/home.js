ce7d990333c1436fac59c6ee44b0a5e2660d52f6
Naveen Singh Badhan <naveenbadhan27@gmail.com>
2021-07-14 10:02:53 +0530

    Create home.js

---
+++ react1/component/home.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+
+function Home(){
+	return(
+		<>
+		<div style={{height:'1000px', background:'red'}}>
+
+			<h1>Welcome to Home</h1>
+		</div>
+
+		</>
+	)
+}
+
+
+export default Home;
