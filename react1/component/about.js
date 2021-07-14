66b1f59938c9d77c4cf0b4691f67230d5477a436
Naveen Singh Badhan <naveenbadhan27@gmail.com>
2021-07-14 10:02:50 +0530

    Create about.js

---
+++ react1/component/about.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+
+function About(){
+	return(
+		<>
+		<div style={{height:'1000px', background:'blue'}}>
+
+			<h1>Welcome to About</h1>
+
+		</div>
+
+		</>
+	)
+}
+
+
+export default About;
