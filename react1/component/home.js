7e53a14369bdaa462699ffe2f6a760eb3aadc2b0
Naveen Singh Badhan <naveenbadhan27@gmail.com>
2021-07-16 11:04:57 +0530

    Update home.js

--- react1/component/home.js
+++ react1/component/home.js
@@ -1,12 +1,94 @@
-import React from 'react';
-
-
+import React,{useState, useEffect} from 'react';
+import {Link} from 'react-router-dom';
+import Navbar from './navbar';
+import Axios from 'axios';
+import { func } from 'prop-types';
 function Home(){
+
+	const [name, setName] = useState('');
+
+
+function frmsubmit(){
+	var email = 'abc@gmail.com';
+	var password = '123456789';
+	// alert('hi '+name);
+	// localStorage.setItem('token',name);
+	// window.location.href='/about';
+	Axios.post('http://localhost:30/insertdata', {
+		Name:name,
+		Email:email,
+		Password:password
+	}).then(function(succ){
+		// console.log(succ);
+		if(succ.data == true){
+			alert('Yes');
+			getuserdata();
+		}else{
+			alert('No');
+		}
+	})
+}
+
+function frmsubmits(x){
+	alert(x);
+}
+
+const [users, setUsers] = useState([]);
+ 
+function getuserdata(){
+	Axios.get('http://localhost:30/fetchdata').then(function(succ){
+		// console.log(succ.data)
+		setUsers(succ.data);
+	})
+}
+
+
+useEffect(() => {
+	getuserdata();
+}, [])
+
+
+
+
+
 	return(
 		<>
-		<div style={{height:'1000px', background:'red'}}>
+		<Navbar />
+		<div className="col-lg-12">
 
 			<h1>Welcome to Home</h1>
+			<div className="col-lg-4 col-lg-offset-2">
+				<div className="form-group">
+					<input type='text' id='name' className='form-control' onChange={(event) => {setName(event.target.value)}} placeholder="Fill your name" />
+				</div>
+				<div className="form-group">
+					<button className="btn btn-info" onClick={frmsubmit}>Click</button>
+					<button className="btn btn-danger" onClick={() => frmsubmits('Hello')}>Click</button>					
+				</div>
+
+			</div>
+
+			<div className="col-lg-6">
+				<table className="table table-hover table-bordered">
+					<thead>
+						<tr>
+							<td>Name</td>
+							<td>Email</td>
+							<td>Password</td>
+						</tr>
+					</thead>
+					<tbody>
+						{users.map((row) => (
+							<tr key={row._id}>
+								<td>{row.Name}</td>
+								<td>{row.Email}</td>
+								<td>{row.Password}</td>
+							</tr>
+						))}
+					</tbody>
+				</table>
+			</div>
+
 		</div>
 
 		</>
