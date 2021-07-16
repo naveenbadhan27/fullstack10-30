9859537257eefc3fd01ef8d6a749bbb07061d391
Naveen Singh Badhan <naveenbadhan27@gmail.com>
2021-07-16 11:04:54 +0530

    Update about.js

--- react1/component/about.js
+++ react1/component/about.js
@@ -1,12 +1,35 @@
-import React from 'react';
-
-
+import React, {useState, useEffect} from 'react';
+import {Link} from 'react-router-dom';
+import Navbar from "./navbar";
 function About(){
+
+	const token = localStorage.getItem('token');
+	if(token == null){
+		window.location.href='/';
+	}
+
+	const [name, setName] = useState('');
+
+	function session(){
+		setName(token);
+	}
+
+	useEffect(() => {
+		session();
+	}, [])
+
+	function logout(){
+		localStorage.removeItem('token');
+		window.location.href='/';
+	}
+
+
 	return(
 		<>
-		<div style={{height:'1000px', background:'blue'}}>
-
-			<h1>Welcome to About</h1>
+		<Navbar />
+		<div>
+		<button className="btn btn-danger" onClick={logout}>Logout</button>
+			<h1>Welcome {name}</h1>
 
 		</div>
 
